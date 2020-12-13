10aff81ea74379cccc373a70947ac7c729119d7c
Joshua Mathew Philip <joshuap2000@gmail.com>
2020-12-13 11:55:10 +0530

    Create chatboxtest.py

---
+++ chatboxtest.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/joshuaphilip/Desktop/chromedriver')
+
+driver.get("https://node-red-qoajg-2020-10-28.eu-gb.mybluemix.net/ui/#!/0?socketid=BDCALMAVAR_bBLOSAAAc")
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="WACLauncher__Button"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="downshift-0-toggle-button"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="downshift-0-item-1"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="downshift-6-toggle-button"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="downshift-6-item-3"]/div').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="WAC__message-5"]/div/div/div[3]/div[2]/ul/li[1]/button').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="WAC__message-7"]/div/div/div[3]/div[2]/ul/li[2]/button').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="WAC__message-10"]/div/div/div[2]/div[2]/ul/li[1]/button').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="WAC__message-13"]/div/div/div[2]/div[2]/ul/li[2]/button').click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="WAC__message-16"]/div/div/div[2]/div[2]/ul/li[3]/button').click()
